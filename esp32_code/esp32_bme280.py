7a3d2c474d9ffa30482c4f50395a57222edf044d
Takumi Hirota <hirota.takumi.uitec@gmail.com>
2022-02-07 09:21:33 +0900

    commit

--- esp32_code/esp32_bme280.py
+++ esp32_code/esp32_bme280.py
@@ -1,7 +1,10 @@
 from mpy_bme280_esp8266 import bme280
 from machine import Pin, I2C
+from time import sleep
 
 i2c = I2C(scl=Pin(4), sda=Pin(5))
-bme = bme280.BME280(i2c=i2c)
 
-print(bme.values)
+while True:
+    bme = bme280.BME280(i2c=i2c)
+    print(bme.values[0], bme.values[1], bme.values[2])
+    sleep(1.0)
